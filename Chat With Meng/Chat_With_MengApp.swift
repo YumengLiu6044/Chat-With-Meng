93df9bd53d6e829660ddcf583437425561caf87f
Yumeng Liu <lym20041026@gmail.com>
2024-07-11 11:04:37 -0700

    Initial Commit

---
+++ Chat With Meng/Chat_With_MengApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_With_MengApp.swift
+//  Chat With Meng
+//
+//  Created by Yumeng Liu on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Chat_With_MengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
