41ce0f5dcbdf1cf16a88dfdecc43d32634ec73a4
Yumeng Liu <lym20041026@gmail.com>
2024-10-07 14:37:37 -0700

    Added UI for PasswordResetView

--- Chat With Meng/Views/Login/PasswordResetView.swift
+++ Chat With Meng/Views/Login/PasswordResetView.swift
@@ -8,25 +8,72 @@
 import SwiftUI
 
 struct PasswordResetView: View {
-    @Binding var isForgetPassword: Bool
+    
+    @Binding var isForgetPassword:  Bool
+    
+    @State private var email:       String      =   ""
+    
+    @State private var width:       CGFloat     =   100
+    @State private var height:      CGFloat     =   100
+    
+    @State private var toast:       Toast?      =   nil
     
     var body: some View {
-        VStack{
-            HStack {
-                Text("Forgot Password")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                Spacer()
-                Button {
-                    isForgetPassword = false
-                } label: {
-                    Text("Cancel")
+        GeometryReader{
+            geometry in
+            VStack(alignment: .leading, spacing: height * 0.02) {
+                HStack {
+                    Text("Forgot Password")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Button {
+                        isForgetPassword = false
+                    } label: {
+                        Text("Cancel")
+                    }
                 }
+                .padding()
+                
+                Text("Input your email below, if it exists, a reset link will be sent to it")
+                    .font(.body)
+                    .fontWeight(.semibold)
+                    .padding()
+                
+                TextField("Email", text: $email, prompt: Text("Email").foregroundStyle(.gray))
+                    .frame(height: height * 0.02)
+                    .padding()
+                    .background(.ultraThickMaterial)
+                    .clipShape(.rect(cornerRadius: width * 0.03))
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .keyboardType(.asciiCapable)
+                
+                Button {
+                    toast = Toast(style: .success, message: "Email sent")
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Send reset email")
+                            .font(.title2)
+                            
+                        Spacer()
+                    }
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle(radius: width * 0.03))
+                
+                Spacer()
             }
             .padding()
-            
-            Spacer()
+            .onAppear {
+                width = geometry.size.width
+                height = geometry.size.height
+            }
+            .toastView(toast: $toast)
         }
+        
         
         
     }
